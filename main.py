8c761fb1c921e8f7d443691a11e3c2cbd86f76b3
Olivier998 <59978327+Olivier998@users.noreply.github.com>
2020-12-04 15:10:58 -0500

    Removed useless code

--- main.py
+++ main.py
@@ -43,24 +43,6 @@ def main():
     # create interface
     Visualisation.main_box.show_interface(holder)
 
-    import Classification.methodsTester
-    #Classification.methodsTester.find_parameters_list("SGD",
-    #                                                  holder)
-
-    # set classification method
-    # for m in range(2):  # m=3 takes too much time
-    #     for lambd_i in [2**-n for n in range(0, 10)]:
-    #         classifier = Classification.classifier.Classifier(method, m=m, lambd=lambd_i)
-    #         classifier.train(data_train, label_train)
-    #         predicted = classifier.predict(data_valid)
-    #         print("m={}, lambda={}, accuracy={}".format(m, lambd_i, classifier.score(label_valid)))
-
-
-    # classifier = Classification.classifier.Classifier(method, m=1, lambd=0.01)
-    # classifier.train(data_train, label_train)
-    # predicted = classifier.predict(data_valid)
-    # print(classifier.score(label_valid))
-
 
 if __name__ == "__main__":
     main()
